6f1cf7521684f864041722fdda738f0564697712
eduardotarik <eduardotarik@outlook.pt>
2023-05-28 17:14:40 +0100

    SeedDb

--- SuperShop2/Startup.cs
+++ SuperShop2/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SuperShop2.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SuperShop2
 {
@@ -29,6 +24,8 @@ namespace SuperShop2
             {
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddTransient<SeedDb>();
 
             services.AddControllersWithViews();
         }
