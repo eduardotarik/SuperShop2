2b53d9142e5bc2ef5f1435514a23db16c1719603
eduardotarik <eduardotarik@outlook.pt>
2023-05-28 15:59:05 +0100

    First controller

---
+++ SuperShop2/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop2.Data.Entities;
+
+namespace SuperShop2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
